cce831a53c7f1b16798766401e5315288b308432
Sergey Zuichenko <srgdude7@gmail.com>
2017-02-28 20:30:01 +0200

    final commit

--- src/Example.java
+++ src/Example.java
@@ -7,6 +7,13 @@ import java.io.InputStreamReader;
  */
 public class Example {
     public static void main(String[] args) throws IOException {
+        String s = getString();
+        System.out.println(s);
+    }
 
+    public static String getString() throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        return s;
     }
 }
